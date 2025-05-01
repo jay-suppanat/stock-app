20ffaabc042102b493ddbb37397382643293bd20
Suppanat Chinthumrucks <suppanat@siamsquared.com>
2025-05-01 15:24:12 +0700

    Add protocol extension

---
+++ StockApp/StockApp/Support File/Extension/Protocol+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  Protocol+Extension.swift
+//  StockApp
+//
+//  Created by Suppanat Chinthumrucks on 1/5/2568 BE.
+//
+
+protocol LifeCycle {}
+protocol Themes {}
+protocol UserInterface {}
+protocol Service {}
+protocol Logic {}
